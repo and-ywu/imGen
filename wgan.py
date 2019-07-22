454caa0bd1451e48674b64397aa3dd81c11c3749
bunnycide <andywu1000@gmail.com>
2019-07-22 13:16:24 -0700

    wgan improved

--- wgan.py
+++ wgan.py
@@ -25,11 +25,12 @@ timestamp = now.strftime("%Y%m%d%H%M%S")
 os.makedirs("images/wgan_{}".format(timestamp))
 
 class RandomWeightedAverage(_Merge):
-    def _merge_function(self, inputs):            
-        alpha= K.random_uniform((32, 1, 1, 1))
-        return (alpha * inputs[0])+ ((1-alpha * inputs[1])
+    """Provides a (random) weighted average between real and generated image samples"""
+    def _merge_function(self, inputs):
+        alpha = K.random_uniform((32, 1, 1, 1))
+        return (alpha * inputs[0]) + ((1 - alpha) * inputs[1])
 
-class WGAN():
+class WGANGP():
     def __init__(self):
         self.img_rows = 64
         self.img_cols = 64
@@ -41,34 +42,89 @@ class WGAN():
         self.n_critic = 5
         optimizer = RMSprop(lr=0.00005)
 
-        # Build and compile the critic
-        self.critic = self.build_critic()
-        self.critic.compile(loss=self.wasserstein_loss,
-            optimizer=optimizer,
-            metrics=['accuracy'])
-
-        # Build the generator
+        # Build the generator and critic
         self.generator = self.build_generator()
+        self.critic = self.build_critic()
 
-        # The generator takes noise as input and generated imgs
-        z = Input(shape=(self.latent_dim,))
-        img = self.generator(z)
+        #-------------------------------
+        # Construct Computational Graph
+        #       for the Critic
+        #-------------------------------
 
-        # For the combined model we will only train the generator
+        # Freeze generator's layers while training critic
+        self.generator.trainable = False
+
+        # Image input (real sample)
+        real_img = Input(shape=self.img_shape)
+
+        # Noise input
+        z_disc = Input(shape=(self.latent_dim,))
+        # Generate image based of noise (fake sample)
+        fake_img = self.generator(z_disc)
+
+        # Discriminator determines validity of the real and fake images
+        fake = self.critic(fake_img)
+        valid = self.critic(real_img)
+
+        # Construct weighted average between real and fake images
+        interpolated_img = RandomWeightedAverage()([real_img, fake_img])
+        # Determine validity of weighted sample
+        validity_interpolated = self.critic(interpolated_img)
+
+        # Use Python partial to provide loss function with additional
+        # 'averaged_samples' argument
+        partial_gp_loss = partial(self.gradient_penalty_loss,
+                          averaged_samples=interpolated_img)
+        partial_gp_loss.__name__ = 'gradient_penalty' # Keras requires function names
+
+        self.critic_model = Model(inputs=[real_img, z_disc],
+                            outputs=[valid, fake, validity_interpolated])
+        self.critic_model.compile(loss=[self.wasserstein_loss,
+                                              self.wasserstein_loss,
+                                              partial_gp_loss],
+                                        optimizer=optimizer,
+                                        loss_weights=[1, 1, 10])
+        #-------------------------------
+        # Construct Computational Graph
+        #         for Generator
+        #-------------------------------
+
+        # For the generator we freeze the critic's layers
         self.critic.trainable = False
+        self.generator.trainable = True
 
-        # The critic takes generated images as input and determines validity
+        # Sampled noise for input to generator
+        z_gen = Input(shape=(100,))
+        # Generate images based of noise
+        img = self.generator(z_gen)
+        # Discriminator determines validity
         valid = self.critic(img)
+        # Defines generator model
+        self.generator_model = Model(z_gen, valid)
+        self.generator_model.compile(loss=self.wasserstein_loss, optimizer=optimizer)
 
-        # The combined model  (stacked generator and critic)
-        self.combined = Model(z, valid)
-        self.combined.compile(loss=self.wasserstein_loss,
-            optimizer=optimizer,
-            metrics=['accuracy'])
-        print("built combined")
+
+    def gradient_penalty_loss(self, y_true, y_pred, averaged_samples):
+        """
+        Computes gradient penalty based on prediction and weighted real / fake samples
+        """
+        gradients = K.gradients(y_pred, averaged_samples)[0]
+        # compute the euclidean norm by squaring ...
+        gradients_sqr = K.square(gradients)
+        #   ... summing over the rows ...
+        gradients_sqr_sum = K.sum(gradients_sqr,
+                                  axis=np.arange(1, len(gradients_sqr.shape)))
+        #   ... and sqrt
+        gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+        # compute lambda * (1 - ||grad||)^2 still for each single sample
+        gradient_penalty = K.square(1 - gradient_l2_norm)
+        # return the mean as loss over all the batch samples
+        return K.mean(gradient_penalty)
+
 
     def wasserstein_loss(self, y_true, y_pred):
         return K.mean(y_true * y_pred)
+
 
     def build_generator(self):
         model = Sequential()
@@ -135,8 +191,8 @@ class WGAN():
 
         # Adversarial ground truths
         valid = -np.ones((batch_size, 1))
-        fake = np.ones((batch_size, 1))
-
+        fake =  np.ones((batch_size, 1))
+        dummy = np.zeros((batch_size, 1)) # Dummy gt for gradient penalty
         for epoch in range(epochs):
 
             for _ in range(self.n_critic):
@@ -148,41 +204,28 @@ class WGAN():
                 # Select a random batch of images
                 idx = np.random.randint(0, X_train.shape[0], batch_size)
                 imgs = X_train[idx]
-                
-                # Sample noise as generator input
+                # Sample generator input
                 noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-
-                # Generate a batch of new images
-                gen_imgs = self.generator.predict(noise)
-
                 # Train the critic
-                d_loss_real = self.critic.train_on_batch(imgs, valid)
-                d_loss_fake = self.critic.train_on_batch(gen_imgs, fake)
-                d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
-
-                # Clip critic weights
-                for l in self.critic.layers:
-                    weights = l.get_weights()
-                    weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
-                    l.set_weights(weights)
-
+                d_loss = self.critic_model.train_on_batch([imgs, noise],
+                                                                [valid, fake, dummy])
 
             # ---------------------
             #  Train Generator
             # ---------------------
 
-            g_loss = self.combined.train_on_batch(noise, valid)
+            g_loss = self.generator_model.train_on_batch(noise, valid)
 
             if epoch % sample_interval == 0:
                 self.log(epoch, d_loss, g_loss)
         self.log(epoch+1, d_loss, g_loss)
-        
-            
+
+
     def log(self, epoch, d_loss, g_loss):
-        print ("%d [D: %f] [G: %f]" 
+        print ("%d [D: %f] [G: %f]"
                % (epoch, d_loss[0], g_loss[0]))
         self.sample_images(epoch)
-        
+
     def sample_images(self, epoch):
         r,c = 5,5
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
@@ -199,4 +242,4 @@ class WGAN():
         plt.close()
 
 wgan = WGAN()
-wgan.train(epochs=10000, batch_size=32, sample_interval=100)
+wgan.train(epochs=30000, batch_size=32, sample_interval=100)
